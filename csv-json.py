10911647cfb88cb1e4db833994534aafec744fe6
Alvyn McQuitty <alvynmcq@gmail.com>
2014-03-25 23:49:20 +0000

    made it a function
    can now tell it which file to convert from the command line. I am sure
    this isn’t the most pythonic way of doing this. But it works.

--- csv-json.py
+++ csv-json.py
@@ -1,11 +1,23 @@
 import csv
+import sys
 import json
 
-csvfile = open('serial.csv', 'r')
-jsonfile = open('serial.json', 'w')
+def csvToJson(csvfile):
+	csv_filename = csvfile[0]
+	print("Opening .csv file: ",csv_filename)
+	csvfile = open(csv_filename, 'r')
+	reader = csv.DictReader(csvfile)
 
-fieldnames = ("StationNumber","SerialNumber")
-reader = csv.DictReader(csvfile, fieldnames)
-for row in reader:
-	json.dump(row, jsonfile, sort_keys=True, indent=2)
-	jsonfile.write('\n')
+	json_filename = csv_filename.split(".") [0]+".json"
+	print("Saving .json to file: ",json_filename)
+	jsonfile = open(json_filename, 'w')
+    
+	for row in reader:
+	    json.dump(row, jsonfile, sort_keys=True, indent=2)
+	    jsonfile.write('\n')
+
+	csvfile.close()
+	jsonfile.close()
+
+if __name__=="__main__":
+	csvToJson(sys.argv[1:])
