72f912b00f9a1f0b9eaf50f83d2aeab42eedae1d
Alvyn McQuitty <alvynmcq@gmail.com>
2014-03-31 23:36:12 +0100

    C# Version of serial number script
    Decided to try some C# programming. Program is just a rewrite of the
    serial.py script.

---
+++ SerialNumber/SerialNumber/SerialNumber/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate a sequence of integers from 1 to 147  
+            // this represents the station number
+            IEnumerable<int> machine_number = Enumerable.Range(1, 147);
+            // List to write station name strings into
+            List<string> workstation_names = new List<string>();
+            // Write station names to list
+            foreach (int num in machine_number)
+            {
+                if (num < 10)
+                    workstation_names.Add("3420094-STN00" + num);
+                else if (num < 100)
+                    workstation_names.Add("3420094-STN0" + num);
+                else
+                    workstation_names.Add("3240094-STN" + num);
+            }
+            // WMI query machines for details
+            foreach (string names in workstation_names)
+            {
+                try
+                {
+                    ManagementScope scope = new ManagementScope(@"\\" + names + @"\root\cimv2");
+                    scope.Connect();
+
+                    ObjectQuery details = new ObjectQuery("SELECT * FROM Win32_Bios");
+
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, details);
+                    ManagementObjectCollection queryCollection = searcher.Get();
+
+                    foreach (ManagementObject stuff in queryCollection)
+                    {
+                        Console.WriteLine("-------------------");
+                        Console.WriteLine("Details");
+                        Console.WriteLine("-------------------");
+                        Console.WriteLine("StationNumber: {0}", names);
+                        Console.WriteLine("SerialNumber: {0}", stuff["SerialNumber"]);
+                        Console.WriteLine("-------------------");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Machine not responding.");
+                }
+            }
+        }
+    }
+}
